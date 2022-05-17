b58cafbe6badadbc34218e1025a67176d077e75a
Shadi <89422699+shadiwin@users.noreply.github.com>
2022-05-17 14:04:40 +0100

    step 1

--- server.js
+++ server.js
@@ -3,18 +3,104 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+//body parser:
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+const bookings = [
+  {
+    id: 1,
+    title: "Mr",
+    firstName: "Jimi",
+    surname: "Hendrix",
+    email: "jimi@example.com",
+    roomId: 2,
+    checkInDate: "2017-11-21",
+    checkOutDate: "2017-11-23",
+  },
+  {
+    id: 2,
+    title: "King",
+    firstName: "James",
+    surname: "Brown",
+    email: "jamesbrown@example.com",
+    roomId: 1,
+    checkInDate: "2018-02-15",
+    checkOutDate: "2018-02-28",
+  },
+  {
+    id: 3,
+    title: "Queen",
+    firstName: "Aretha",
+    surname: "Franklin",
+    email: "aretha@example.com",
+    roomId: 5,
+    checkInDate: "2018-03-01",
+    checkOutDate: "2018-04-09",
+  },
+  {
+    id: 4,
+    title: "Mr",
+    firstName: "Stevie",
+    surname: "Wonder",
+    email: "stevie@example.com",
+    roomId: 6,
+    checkInDate: "2017-12-25",
+    checkOutDate: "2018-01-03",
+  },
+  {
+    id: 5,
+    title: "Mr",
+    firstName: "John",
+    surname: "Lennon",
+    email: "lennon@example.com",
+    roomId: 3,
+    checkInDate: "2017-08-30",
+    checkOutDate: "2017-10-02",
+  },
+];
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+app.get("/", (req, res) => {
+  res.send("Hotel booking server.  Ask for /bookings, etc.");
 });
+
+//Create a new booking:
+app.post("/bookings", (req, res) => {
+  const {
+    title,
+    firstName,
+    surname,
+    email,
+    roomId,
+    checkInDate,
+    checkOutDate,
+  } = req.body;
+  const bookingObject = {
+    id: bookings.length,
+    title,
+    firstName,
+    surname,
+    email,
+    roomId,
+    checkInDate,
+    checkOutDate,
+  };
+  bookings.push(bookingObject);
+  res.send(bookings);
+});
+
+//Read all bookings
+// app.get("./bookings", (req, res) => {
+//   res.json(bookings);
+// });
+
+//Read one booking, specified by an ID
+
+//Delete a booking, specified by an ID
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 4000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
